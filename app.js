5ec1c06a8e5127a6c4b4f4f77d5aaf7d47d70d3a
Your Name <yourgmail address>
2023-03-01 16:09:38 +0300

    hashPassword-env-add

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.CONNECTION)
+
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    })
+
+    newUser.save().then(() => {
+        res.render("secrets");
+    }).catch((err) => {
+        console.log(err);
+    })
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = md5(req.body.password)
+
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        })
+        .catch((error) => {
+            //When there are errors We handle them here
+
+            console.log(err);
+            res.send(400, "Bad Request");
+        });
+
+})
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+});
